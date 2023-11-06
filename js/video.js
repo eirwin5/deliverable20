b7887182c830e10d86f9c9cb89c19fd8446821a9
eirwin <eirwin@umich.edu>
2023-11-06 15:30:38 -0500

    fully functioning

--- js/video.js
+++ js/video.js
@@ -2,21 +2,68 @@ var video;
 
 window.addEventListener("load", function () {
   console.log("Good job opening the window");
-  video = document.querySelector("video");
+  video = document.querySelector("#player1");
   video.autoplay = false;
+  video.pause(); 
   video.loop = false;
 
   document.querySelector("#play").addEventListener("click", function () {
-	console.log("Play Video");
-	console.log(video);
-	video.play();
+    console.log("Play Video");
+    video.play();
   });
 
   document.querySelector("#pause").addEventListener("click", function () {
-	console.log("Pause Video");
-	console.log(video);
-	video.pause();
+    console.log("Pause Video");
+    video.pause();
   });
 
-	// Slow the current video speed by 10% each time the button is clicked and log the new speed to the console.  
+  // Slow the current video speed by 10% each time the button is clicked and log the new speed to the console.
+  this.document.querySelector("#slower").addEventListener("click", function () {
+    console.log("Slow Video");
+    video.playbackRate = video.playbackRate * 0.9;
+    console.log(video.playbackRate);
+  });
+
+  // Increase the current video speed each time the button is clicked and log the new speed to the console.
+  // Change it by an amount proportional to the slow down. CHECK THIS!!
+  // If you slow down three times and then speed up three times you should be within 5 digits of 100% again.
+  this.document.querySelector("#faster").addEventListener("click", function () {
+    console.log("Speed Video");
+    video.playbackRate = video.playbackRate * 1.1;
+    console.log(video.playbackRate);
+  });
+
+  // Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start of the video - no farther.
+  // Log the current location of the video.
+  this.document.querySelector("#skip").addEventListener("click", function () {
+    console.log("Skip ahead");
+    console.log(video.currentTime);
+    video.currentTime =
+      video.currentTime + 10 < video.duration ? video.currentTime + 10 : 0;
+  });
+
+  // Mute/unmute the video and update the text in the button.
+  this.document.querySelector("#mute").addEventListener("click", function () {
+    console.log("Mute Video");
+	console.log(video.muted); 
+      video.muted = !video.muted; 
+  });
+
+  // Change the volume based on the slider and update the volume information.
+  this.document.querySelector("#slider").addEventListener("input", function () {
+    console.log("Change volume");
+	console.log(video.volume); 
+	video.volume = this.value / 100; 
+	document.querySelector("#volume").innerHTML = this.value + ' %'; 
+  });
+
+  // Styled: Utilize the existing oldSchool class on the video element
+  this.document.querySelector("#vintage").addEventListener("click", function () { 
+	video.classList.add("oldSchool"); 
+  })
+  
+  // Original: Remove the oldSchool class from the video.
+  this.document.querySelector("#orig").addEventListener("click", function () { 
+	video.classList.remove("oldSchool"); 
+  })
 });
