029d67d953c1ac64a4001a64d7924795f5ba0a81
eirwin <eirwin@umich.edu>
2023-11-06 09:29:31 -0500

    initial attempt

--- js/video.js
+++ js/video.js
@@ -1,11 +1,20 @@
 var video;
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+window.addEventListener("load", function () {
+  console.log("Good job opening the window");
+  video = document.querySelector("video");
+  video.autoplay = false;
+  video.loop = false;
 
+  document.querySelector("#play").addEventListener("click", function () {
+	console.log("Play Video");
+	console.log(video);
+	video.play();
+  });
+
+  document.querySelector("#pause").addEventListener("click", function () {
+	console.log("Pause Video");
+	console.log(video);
+	video.pause();
+  });
 });
-
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
